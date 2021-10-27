de55be6d2acb1b603343fec18fa1b2bdd8ac4751
UsmonBarfakov <barfakov@gmail.com>
2021-10-27 08:10:25 +0500

    Changed Even game to work with Engine

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -2,59 +2,29 @@
 
 namespace BrainGames\Even;
 
-use function cli\line;
-use function cli\prompt;
+use function BrainGames\Engine\runGame;
+
+use const BrainGames\Engine\COUNT_OF_ROUNDS;
 
 function run(): void
 {
-    $countCorrect = 3; // Count of correct answers must be 3
-    $userName = greeting();
-    startGame($userName, $countCorrect);
-}
+    $questionText = 'Answer "yes" if the number is even, otherwise answer "no".';
+    $questions = [];
 
-function greeting(): string
-{
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    for ($i = 0; $i < COUNT_OF_ROUNDS; $i++) {
+        $question = rand(1, 100);
+        $answer = correctAnswer($question);
 
-    return $name;
-}
-
-function startGame($userName, $countCorrect): void
-{
-    showMsg('Answer "yes" if the number is even, otherwise answer "no".');
-    for ($usersCountCorrect = 0; $usersCountCorrect < $countCorrect; $usersCountCorrect++) {
-        $number = getNumber();
-        showMsg("Question: {$number}");
-        $answer = getAnswer("Your answer:");
-
-        if ($answer === checkAnswer($number)) {
-            showMsg("Correct!");
-        } else {
-            showMsg("'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, {$userName}");
-            return;
-        }
+        $questions[] = [
+            'question' => $question,
+            'correctAnswer' => $answer
+        ];
     }
-    showMsg("Congratulations, {$userName}!");
+
+    runGame($questionText, $questions);
 }
 
-function showMsg($text): void
-{
-    line($text);
-}
-
-function getAnswer($text): string
-{
-    return prompt($text);
-}
-
-function getNumber(): int
-{
-    return rand(1, 100);
-}
-
-function checkAnswer($number): string
+function correctAnswer($number): string
 {
     $result = $number % 2;
     if ($result > 0) {
